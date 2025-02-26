09e3ee13c29e79879cf3c12ea20a66a6be93c8c8
ybbotella <151914867+ybbotella@users.noreply.github.com>
2025-02-26 22:06:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './visionariusAgencyFondo.png';
 import './App.css';
 import ChatWidget from './ChatWidget';  // <-- Importa tu componente
 
@@ -8,9 +8,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* Contenido original */}
-        <img src="https://raw.githubusercontent.com/ybbotella/AsistenteIA/refs/heads/main/public/VisionariusAgencyFondo.png" className="App-logo" alt="logo" />
-        
-      
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
 
       {/* Aquí se monta el widget de Anything LLM */}
