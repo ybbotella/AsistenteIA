d10b6ace56faf44342350e2026201f1d9ad9530d
ybbotella <151914867+ybbotella@users.noreply.github.com>
2025-02-26 22:27:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ function App() {
       <header className="App-header">
         {/* Nuevo logo de Visionarius Agency */}
         <img src="/LogoVisionarius.png" className="App-logo" alt="Visionarius Agency Logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
 
       {/* Aquí se monta el widget de Anything LLM */}
